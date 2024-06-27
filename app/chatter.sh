aba9b189d078c15bf69ccb1a73371bc2c6854cff
Justin Warwick <justin.warwick@gmail.com>
2024-06-27 09:34:08 -0700

    piper doesn't have SSML, yet, but found out newlines do insert pauses. Add an inline conversion of eplipses to newline, and add a few more messages.txt entries.

--- app/chatter.sh
+++ app/chatter.sh
@@ -1,7 +1,8 @@
 echo "MESSAGE: $MESSAGE"
 export VOICENAME=$(shuf -n 1 voicenames.txt)
 #echo $MESSAGE | piper --model en_US-lessac-medium --output_file input.wav
-echo $MESSAGE | piper --model $VOICENAME --output_file input.wav
+#eventually when SSML is available in piper, we don't need that sed invocation
+echo $MESSAGE | sed 's/\.\.\./\n/g' | piper --model $VOICENAME --output_file input.wav
 sox input.wav -r 22050 input22k.wav ; mv input22k.wav input.wav
 #STD edition
 sox input.wav krssht.wav  rdo.ogg sinc -n 32767 1000-6000 compand 0.3,1 6:-70,-60,-20 -5 -90 0.2 overdrive 10
